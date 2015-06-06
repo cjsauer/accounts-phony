001c6d0376654a5a0789a6da173db2fb9d83cee4
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-06-06 16:24:51 -0400

    Tested the callback function

--- phony-test.js
+++ phony-test.js
@@ -4,3 +4,14 @@ Tinytest.add('Meteor.loginWithPhony()', function (test) {
     test.equal(Meteor.userId(), "phony-user-id");
   }
 });
+
+Tinytest.add('Meteor.loginWithPhony() callback is working', function(test){
+  if(Meteor.isClient) {
+    setTestVar = function(value) { 
+      test.equal(value, 5);
+    };
+    Meteor.loginWithPhony(Phony.user, function() {
+      setTestVar(5);
+    });
+  }
+});
